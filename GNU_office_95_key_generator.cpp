6c109a1d04c5f3992f04bb80725d01a9d4d000f4
Pikksel One <47038939+Pikone@users.noreply.github.com>
2020-07-24 19:43:05 +0300

    Added:
    a selection of the number of keys and conducted a small refactoring

--- GNU_office_95_key_generator.cpp
+++ GNU_office_95_key_generator.cpp
@@ -2,34 +2,36 @@
 #include <cstdlib>
 #include <ctime>
 
-int main ()
+int main (int argc, char *argv[])
 {
+	if (argc == 1)
+		argv[1] = "1";
+
+	std::cout << argv[1] << " keys for Microsoft Office 95" << std::endl;
 	srand(time(0));
 
-	int key_p1[3];
 	int key_p2[7];
 	int valid = 0;
 
 	int i;
 
-	for(i = 0; i < 3; i++)
-		key_p1[i] = rand() % 10;
+	for (int u = 0; u < atoi(argv[1]); u++){
+		for(i = 0; i < 3; i++)
+			std::cout << rand() % 10;
 
-	do{
-		valid = 0;
+		do{
+			valid = 0;
+			for (i = 0; i < 7; i++)
+			{
+				key_p2[i] = rand() % 10;
+				valid += key_p2[i];
+			}
+		}while (valid % 7 != 0);
+
+		std::cout << "-";
 		for (i = 0; i < 7; i++)
-		{
-			key_p2[i] = rand() % 10;
-			valid += key_p2[i];
-		}
-	}while (valid % 7 != 0);
-
-	for (i = 0; i < 3; i++)
-		std::cout << key_p1[i];
-	std::cout << "-";
-	for (i = 0; i < 7; i++)
-		std::cout << key_p2[i];
-	std::cout << std::endl;
-
+			std::cout << key_p2[i];
+		std::cout << std::endl;
+	}
 	return 0;
 }
